796d6318dff64eb7182ed1edbe69c824e49adace
nsesupport <caniedozie@nse.com.ng>
2016-04-09 20:04:52 +0100

    Added files via upload

---
+++ NSEMarketDataAPICSharpExample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NSEMarketDataAPICSharpExample
+{
+    class Program
+    {
+        //get this from NSE Market Data API website
+        readonly static string NSE_BASEURI = "http://marketdataapi.nse.com.ng/v1/odata/";
+        
+        //issued to you by NSE on completion of onboarding
+        readonly static string USER_NAME = "tester";
+        readonly static string USER_TOKEN = "6xxxxd01-8xx0-4468-9xxx9-453959f7xxxx";
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var stockQuoteClient = Task.Run(() => new StockQuoteClient().FetchTypedWith(NSE_BASEURI, USER_NAME, USER_TOKEN));
+                
+                // Output a message from the calling thread.
+                Console.WriteLine(DateTime.Now.ToString() + ": StockQuote data request sent to NSE");
+                stockQuoteClient.Wait();
+
+                Thread.Sleep(60000);
+            }
+        }
+    }
+}
---
+++ NSEMarketDataAPICSharpExample/StockQuote.cs
@@ -0,0 +1,66 @@
+﻿using Simple.OData.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSEMarketDataAPICSharpExample
+{
+    class StockQuote
+    {
+        public string Symbol { set; get; }
+        public double Price { set; get; }
+        public DateTime LastTradeTime { set; get; }
+
+        //define members you are interested in here
+    }
+
+    class StockQuoteClient
+    {
+        public async void FetchUntypedWith(string nseBaseURI, string myUserName, string myToken)
+        {
+            var settings = new ODataClientSettings(nseBaseURI);
+            settings.BeforeRequest = (message) =>
+            {
+                String aux = String.Join(":", new String[] { myUserName, myToken });
+                var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(aux);
+                message.Headers.TryAddWithoutValidation("Authorization",
+                "Basic " + System.Convert.ToBase64String(plainTextBytes));
+            };
+
+            var client = new ODataClient(settings);
+            var stockQuote = await client
+                .For("StockQuote")
+                .Key("FBNH")
+                .FindEntryAsync();
+
+            //You can store retrieved data for later use by your client.
+
+            Console.WriteLine(DateTime.Now.ToString() + ": " + stockQuote["Symbol"] + ": " + stockQuote["Price"] + " as @ " + stockQuote["LastTradeTime"]);
+        }
+
+        public async void FetchTypedWith(string nseBaseURI, string myUserName, string myToken)
+        {
+            var settings = new ODataClientSettings(nseBaseURI);
+            settings.BeforeRequest = (message) =>
+            {
+                String aux = String.Join(":", new String[] { myUserName, myToken });
+                var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(aux);
+                message.Headers.TryAddWithoutValidation("Authorization",
+                "Basic " + System.Convert.ToBase64String(plainTextBytes));
+            };
+
+            var client = new ODataClient(settings);
+            var stockQuote = await client
+                .For<StockQuote>()
+                .Key("FBNH")
+                .FindEntryAsync();
+
+            //You can store retrieved data for later use by your client.
+
+            Console.WriteLine(DateTime.Now.ToString() + ": "+ stockQuote.Symbol + ": " + stockQuote.Price.ToString("N02") + " as @ " + stockQuote.LastTradeTime.ToString());
+        }
+    }
+}
